2760129f0d3b207465845e09f02a2296d14dbf8b
Phong <dotienphong1993@gmail.com>
2024-04-29 10:57:59 +0700

    update code

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ await app.register(cors, {
 // Rate-Limit
 app.register(rateLimit, {
   timeWindow: 5 * 60 * 1000, // 5 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+  max: 1000, // Limit each IP to 1000 requests per `window` (here, per 5 minutes).
   statusCode: 429,
   error: "Too Many Requests",
   message: "Too many requests from this IP 🔥🔥, please try again later !",
