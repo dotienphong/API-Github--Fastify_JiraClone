d0224b91ff3a82211b1a8848bf2d7d5b3e944345
Phong <dotienphong1993@gmail.com>
2024-06-09 19:43:17 +0700

    update

--- src/connection/database.connection.js
+++ src/connection/database.connection.js
@@ -2,11 +2,11 @@ const {Pool} = require("pg");
 
 // pg configuration Local
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || "weblearn.ddns.net",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "admin",
+  database: process.env.DB_DATABASE || "postgres",
+  port: process.env.DB_PORT || "5432",
   max: 100, // Số lượng kết nối tối đa trong pool
   idleTimeoutMillis: 10000, // Thời gian chờ để giải phóng kết nối không sử dụng
   connectionTimeoutMillis: 2000, // Thời gian chờ để thiết lập kết nối
