362942faf1fd89245ff3c64d7ec9a95babd25f00
Phong <dotienphong1993@gmail.com>
2024-06-09 19:24:32 +0700

    update

--- src/utils/queryDatabase.js
+++ src/utils/queryDatabase.js
@@ -1,5 +1,15 @@
-const db = require("../connection/database.connection");
 const logger = require("../loggers/loggers.config");
+
+const db = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  max: 100, // Số lượng kết nối tối đa trong pool
+  idleTimeoutMillis: 10000, // Thời gian chờ để giải phóng kết nối không sử dụng
+  connectionTimeoutMillis: 2000, // Thời gian chờ để thiết lập kết nối
+});
 
 const QueryDatabase = async (sql) => {
   try {
