b46a5c5dbf98851e906e526b946fda427a78b93b
Phong <dotienphong1993@gmail.com>
2024-06-11 21:47:20 +0700

    update

--- src/utils/generateJWT.js
+++ src/utils/generateJWT.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const GenerateAccessToken = (payload) => {
   return jwt.sign(payload, process.env.SECRET_TOKEN || "c8a4Pb92H854O4679N2f9d700G50295a7d1adad76d1d1fd37c34273b3fa4", {
-    expiresIn: process.env.EXPIRES_ACCESS_TOKEN || "31d",
+    expiresIn: process.env.EXPIRES_ACCESS_TOKEN || "1d",
   });
 };
 
