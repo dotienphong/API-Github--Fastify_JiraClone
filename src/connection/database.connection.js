a4acf51f257074a2195cda6793d4d6cde8e415f9
Phong <dotienphong1993@gmail.com>
2024-09-21 09:29:43 +0700

    update

--- src/connection/database.connection.js
+++ src/connection/database.connection.js
@@ -9,8 +9,8 @@ const db = new Pool({
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
   max: 1000, // Số lượng kết nối tối đa trong pool
-  idleTimeoutMillis: 2000, // Thời gian chờ để giải phóng kết nối không sử dụng, thời gian chờ idle
-  connectionTimeoutMillis: 5000, // Thời gian chờ để thiết lập kết nối
+  idleTimeoutMillis: 1500, // Thời gian chờ để giải phóng kết nối không sử dụng, thời gian chờ idle
+  connectionTimeoutMillis: 10000, // Thời gian chờ để thiết lập kết nối
 });
 
 db.on("connect", (client) => {
