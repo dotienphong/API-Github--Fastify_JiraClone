44a8444ca7eb8a6af277debb69f1f2ab3edbd81c
Phong <dotienphong1993@gmail.com>
2024-09-24 22:39:58 +0700

    update

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,7 +1,7 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('Math Tests', function() {
-  it('should add 1 + 2 correctly', function() {
+describe("Math Tests", function () {
+  it("should add 1 + 2 correctly", function () {
     assert.strictEqual(1 + 2, 3);
   });
 });
