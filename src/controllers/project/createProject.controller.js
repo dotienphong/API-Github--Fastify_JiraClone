216b6ae48b84fc686a00d5b6c78ab63efdb31653
Phong <dotienphong1993@gmail.com>
2024-09-27 22:23:46 +0700

    update

--- src/controllers/project/createProject.controller.js
+++ src/controllers/project/createProject.controller.js
@@ -20,7 +20,7 @@ const CreateProject = async (req, res, next) => {
 
     if (!name || !payment || !time_start || !time_end || !note || !priority) {
       res.status(400);
-      return {code: 400, message: "Missing field"}
+      return {code: 400, message: "Missing field"};
     }
 
     const sql = `
