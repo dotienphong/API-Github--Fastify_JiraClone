2d1b8bbc052a84837a7b86a82421addabc6df3c8
Phong <dotienphong1993@gmail.com>
2024-09-29 10:48:36 +0700

    update

--- src/connection/firebase/firebase.connection.js
+++ src/connection/firebase/firebase.connection.js
@@ -1,6 +1,5 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./jiraclone-a750e-firebase-adminsdk-lghhc-03012998cc.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
