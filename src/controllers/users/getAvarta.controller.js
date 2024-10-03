7a19a582c41848855823c5aa8be2a0a5f303bf1e
Phong <dotienphong1993@gmail.com>
2024-10-03 11:23:25 +0700

    update

--- src/controllers/users/getAvarta.controller.js
+++ src/controllers/users/getAvarta.controller.js
@@ -4,25 +4,25 @@ const mime = require("mime-types");
 
 const uploadDir = path.resolve("./uploads");
 
-const GetAvarta = async (req, reply) => {
+const GetAvarta = async (req, res) => {
   const {filename} = req.params;
   const filePath = path.join(uploadDir, filename);
 
   if (fs.existsSync(filePath)) {
     const contentType = mime.lookup(filePath);
-    reply.header("Content-Type", contentType || "application/octet-stream");
+    res.header("Content-Type", contentType || "application/octet-stream");
     const stream = fs.createReadStream(filePath);
     stream.on("error", (err) => {
-      reply.status(500).send({error: "Error reading file"});
+      res.status(500).send({error: "Error reading file"});
     });
 
     stream.on("close", () => {
-      return reply;
+      return res;
     });
 
-    return reply.send(stream);
+    return res.send(stream);
   } else {
-    return reply.status(404).send({error: "File or directory not found"});
+    return res.status(404).send({error: "File or directory not found"});
   }
 };
 
--- src/controllers/users/uploadAvarta.controller.js
+++ src/controllers/users/uploadAvarta.controller.js
@@ -1,22 +1,38 @@
 const path = require("path");
+const jwt = require("jsonwebtoken");
 
-const UploadAvarta = async (req, reply) => {
+const UploadAvarta = async (req, res) => {
+  const {authorization} = req.headers;
+  const accessToken = authorization.slice(7);
+  const decodedToken = jwt.decode(accessToken);
+  const {email} = decodedToken;
+  console.log("email", email);
+
   try {
     const file = req.file;
     if (!file) {
-      return reply.status(400).send({error: "No file uploaded"});
+      return res.status(400).send({error: "No file uploaded"});
     }
 
     const filePath = path.join("/avarta", file.filename);
     const formattedPath = filePath.replace(/\\/g, "/");
+    const fileAvartaPath = `${process.env.BASE_URL}:${process.env.PORT}/api${formattedPath}`;
 
-    reply.send({
+    res.send({
       message: "File uploaded successfully",
-      fileAvartaPath: `${process.env.BASE_URL}:${process.env.PORT}/api${formattedPath}`, // Đường dẫn URL của ảnh
+      fileAvartaPath: fileAvartaPath, // Đường dẫn URL của ảnh
       info: req.file,
     });
+
+    // Luu vao database
+    const sqlAddAvartaForUser = `
+        UPDATE "users"
+        SET avarta = '${fileAvartaPath}'
+        WHERE email = '${email}';
+      `;
+    await QueryDatabase(sqlAddAvartaForUser);
   } catch (error) {
-    reply.status(500).send({error: error.message});
+    res.status(500).send({error: error.message});
   }
 };
 
